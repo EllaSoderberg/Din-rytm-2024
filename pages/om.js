56c1cac94c9a5ed65da4d712adf68809aca34aaa
Ella Soderberg <ella.soderberg@kapernikov.com>
2024-04-17 21:47:32 +0200

    finished om

--- pages/om.js
+++ pages/om.js
@@ -1,10 +1,12 @@
 import PageContainer from "@/components/PageContainer";
 import attributes from "@/content/om.md"
 import { Component } from "react";
+import Image from "next/image";
 
 export default class Om extends Component {
     render() {
-        let { aboutme } = attributes
+        let { about, image } = attributes.attributes
+        console.log(attributes.about)
         return (
             <div>
                 <PageContainer>
@@ -13,12 +15,14 @@ export default class Om extends Component {
                             <h1 className="font-serif text-4xl xs:text-6xl">Om Din rytm</h1>
                             <p
                                 className="text-sm py-4"
-                                dangerouslySetInnerHTML={{ __html: aboutme }}
+                                dangerouslySetInnerHTML={{ __html: about }}
                             ></p>
                         </div>
                         <div className="max-w-xs flex justify-center md:w-1/3 self-center">
                             <div className="w-xxl">
-                                {{/*<CompatibleImage imageInfo={text.image} />*/ }}
+                                <Image width={500}
+                                    height={500}
+                                    src={image} />
                             </div>
                         </div>
                     </div>
